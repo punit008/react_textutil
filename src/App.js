6d06c8bd72a99de3b630fb7f584b107139ac00c7
Punit <punitwork18@gmail.com>
2021-08-25 22:02:51 +0530

    added props, proptypes type checking, default props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar';
 
+
 function App() {
   return (
-    <Navbar></Navbar>
+    <>
+     <Navbar title="TextUtils" />
+    </>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,11 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
-        <>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
           <div className="container-fluid">
-            <a className="navbar-brand" href="/">TextUtils</a>
+            <a className="navbar-brand" href="/">{props.title}</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
@@ -15,7 +15,7 @@ export default function Navbar() {
                   <a className="nav-link active" aria-current="page" href="/">Home</a>
                 </li>
                 <li className="nav-item">
-                  <a className="nav-link" href="/">About</a>
+                  <a className="nav-link" href="/">{props.aboutText}</a>
                 </li>
               </ul>
               <form className="d-flex">
@@ -25,6 +25,15 @@ export default function Navbar() {
             </div>
           </div>
         </nav>
-      </>
     )
+}
+
+Navbar.propTypes    =   {
+    title:  PropTypes.string.isRequired,
+    aboutText:  PropTypes.string
+}
+
+Navbar.defaultProps =   {
+    title:  'Set title here',
+    aboutText:  'About'
 }
