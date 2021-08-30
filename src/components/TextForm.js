dc99055887d8eb68a504cd4a688f66240e0d1b9a
Punit <punitwork18@gmail.com>
2021-08-30 22:01:05 +0530

    added logic for textform.js

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -6,19 +6,34 @@ export default function TextForm(props) {
         let newText = text.toUpperCase();
         setText(newText);
     }
-    const handleOnChange =  ()  =>  {
+    const handleLoClick =   ()  =>  {
         console.log("Uppercase was clicked");
-        setText(EventTarget.value);
+        let newText = text.toLowerCase();
+        setText(newText);
     }
-    const [text, setText]   =   useState('Enter text here');
+    const handleOnChange =  (event)  =>  {
+        console.log("Uppercase was clicked");
+        setText(event.target.value);
+    }
+    const [text, setText]  = useState('');
     return (
-        <div>
-            <h1>{ props.heading }</h1>
-            <div className="mb-3">
-                <textarea className="form-control" id="exampleFormControlTextarea1" onChange={handleOnChange} rows="3" value={text}></textarea>
+        <>
+            <div className="container">
+                <h1>{ props.heading }</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" id="exampleFormControlTextarea1" onChange={handleOnChange} rows="3" value={text}></textarea>
+                </div>
+                <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button> &nbsp;
+                <button className="btn btn-primary" onClick={handleLoClick}>Convert to Lowercase</button>
             </div>
-            <button className="btn btn-primary" onClick={handleUpClick}>Convert to uppercase</button>
-        </div>
+            <div className="container my-2">
+                <h1>Your text summary</h1>
+                <p> {text.split(" ").length} words,{text.length} characters </p>
+                <p> {0.008 * text.split(" ").length} Minutes read </p>
+                <h2>Preview</h2>
+                <p>{text}</p>
+            </div>
+        </>
     )
 }
 
